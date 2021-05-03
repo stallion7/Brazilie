5dd4cbd192ed08cb200c80f89cbd8b2989d42f59
stallion7 <stepankrautwurst@gmail.com>
2021-05-03 12:02:45 +0200

    historie
    scrollbar, responzivní background, základ stránky o historii

---
+++ historie.js
@@ -0,0 +1,27 @@
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+const options = {
+    bottom: '23px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true, // default: true
+}
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
